bde3e896e1eb5e792b58d352435c536afaa97445
jimd1989 <jamesdalrymple@protonmail.com>
2021-01-13 16:53:46 +1000

    Checking format string for ambiguity

--- src/Format.hs
+++ src/Format.hs
@@ -1,11 +1,12 @@
-module Format (Format, format) where
+module Format where
 
 -- Concerned with reading the format string into an array of Matcher types,
 -- which tell the program what type of input to expect from the filenames,
 -- what delimiters to look out for, etc.
 
+import Control.Monad (foldM)
 import Data.Set as S (Set, fromList, member)
-import Helpers ((◇), note, tail', toIO)
+import Helpers ((◀), (◁), (◇), note, tail', toIO)
 import Matchers (Matcher(..))
 
 type Format = [Matcher]
@@ -25,7 +26,7 @@ txtAdd (Txt α) ω = Txt $ α ◇ ω
 txtAdd α _       = α
 
 format' ∷ [Matcher] → Matcher → String → Format
-format' ms m []        = reverse (m : ms)
+format' ms m []        = reverse $ m : ms
 format' ms m ('{' : α : '}' : ω)
   | member α numFields = format' (m : ms) (Num $ pure α) ω 
   | member α fields    = format' (m : ms) (Field $ pure α) ω
@@ -35,5 +36,14 @@ format' ms m (α : ω)
   | isTxt m            = format' ms (txtAdd m $ pure α) ω
   | otherwise          = format' (m : ms) (Txt $ pure α) ω
 
+ambiguity' ∷ [Matcher] → Matcher → Either String [Matcher]
+ambiguity' [] α                                = Right [α]
+ambiguity' (β@(Txt _ ) : ω) α  | not $ isTxt α = Right (α : β : ω)
+ambiguity' (β : ω) α@(Txt _)                   = Right (α : β : ω)
+ambiguity' _ _                                 = Left "ambiguous format string"
+
+ambiguity ∷ [Matcher] → Either String [Matcher]
+ambiguity = reverse ◁ foldM ambiguity' []
+
 format ∷ String → IO Format
-format = toIO . note "internal error" . tail' . format' [] Blank
+format = toIO . (ambiguity ◀ note "internal error" . tail' . format' [] Blank)
